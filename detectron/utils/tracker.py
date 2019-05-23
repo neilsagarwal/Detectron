ffb31f387350cdaa9dd6e5ee9be833fb82876b4b
Frank Cangialosi <frankc@csail.mit.edu>
2019-05-23 12:30:32 +0000

    initial object tracking

---
+++ detectron/utils/tracker.py
@@ -0,0 +1,90 @@
+from collections import OrderedDict
+from scipy.spatial.distance import cdist as pairwise_dist
+import numpy as np
+
+def find_centroid(box):
+    x,y,w,h,_ = box
+    x2 = x + w
+    y2 = y + h
+    return (int((x+x2)/2), int((y+y2)/2))
+
+class ObjectTracker(object):
+    MISSING_THRESHOLD = 5
+
+    def __init__(self):
+        self.next_id = 1
+        self.old_obj = OrderedDict()
+        self.box_map = OrderedDict()
+        self.last_seen = OrderedDict()
+
+    def update(self, boxes):
+
+        if len(boxes) == 0:
+            to_forget = []
+            for obj_id in self.last_seen.keys():
+                self.last_seen[obj_id] += 1
+                self.box_map[obj_id] = None
+                if self.last_seen[obj_id] > ObjectTracker.MISSING_THRESHOLD:
+                    to_forget.append(obj_id)
+            for obj_id in to_forget:
+                self.forget(obj_id)
+            return self.box_map
+
+        new_obj = np.zeros((len(boxes), 2), dtype="int")
+        for i in range(len(boxes)):
+            new_obj[i] = find_centroid(boxes[i])
+
+        if len(self.old_obj) == 0:
+            for i in range(0, len(new_obj)):
+                self.register(new_obj[i], boxes[i])
+        else:
+
+            old_ids = list(self.old_obj.keys())
+            old_obj = np.array(list(self.old_obj.values()))
+
+            D = pairwise_dist(old_obj, new_obj)
+
+            old_matched = set()
+            new_matched = set()
+
+            old_ind = D.min(axis=1).argsort()
+            new_ind = D.argmin(axis=1)[old_ind]
+            for (oi, ni) in zip(old_ind, new_ind):
+                if oi in old_matched or ni in new_matched:
+                    continue
+                obj_id = old_ids[oi]
+                self.old_obj[obj_id] = new_obj[ni]
+                self.box_map[obj_id] = boxes[ni]
+                self.last_seen[obj_id] = 0
+                old_matched.add(oi)
+                new_matched.add(ni)
+
+            # potentially disappeared
+            old_not_matched = set(range(0, D.shape[0])).difference(old_matched)
+            # potentially new object
+            new_not_matched = set(range(0, D.shape[1])).difference(new_matched)
+
+            if D.shape[0] >= D.shape[1]:
+                for oi in old_not_matched:
+                    obj_id = old_ids[oi]
+                    self.last_seen[obj_id] += 1
+                    self.box_map[obj_id] = None
+                    if self.last_seen[obj_id] > ObjectTracker.MISSING_THRESHOLD:
+                        self.forget(obj_id)
+            else:
+                for ni in new_not_matched:
+                    self.register(new_obj[ni], boxes[ni])
+
+        return self.box_map
+
+    def register(self, center, box):
+        self.old_obj[self.next_id] = center
+        self.box_map[self.next_id] = box
+        self.last_seen[self.next_id] = 0
+        self.next_id += 1
+
+    def forget(self, obj_id):
+        del self.old_obj[obj_id]
+        del self.box_map[obj_id]
+        del self.last_seen[obj_id]
+
---
+++ tools/track.py
@@ -0,0 +1,354 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2017-present, Facebook, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##############################################################################
+
+"""Perform inference on a single image or all images with a certain extension
+(e.g., .jpg) in a folder.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from collections import defaultdict
+import argparse
+import cv2  # NOQA (Must import before importing caffe2 due to bug in cv2)
+import glob
+import logging
+import os
+import sys
+import time
+from subprocess import Popen
+import shutil
+from tqdm import tqdm
+from termcolor import colored
+
+from caffe2.python import workspace
+
+from detectron.core.config import assert_and_infer_cfg
+from detectron.core.config import cfg
+from detectron.core.config import merge_cfg_from_file
+from detectron.utils.io import cache_url
+from detectron.utils.logging import setup_logging
+from detectron.utils.timer import Timer
+import detectron.core.test_engine as infer_engine
+import detectron.datasets.dummy_datasets as dummy_datasets
+import detectron.utils.c2 as c2_utils
+import detectron.utils.vis as vis_utils
+from detectron.utils.tracker import ObjectTracker
+
+import detectron.utils.env as envu
+envu.set_up_matplotlib()
+import matplotlib.pyplot as plt
+
+import json
+
+import numpy as np
+sys.path.append("/home/ubuntu/sort")
+import sort
+
+c2_utils.import_detectron_ops()
+
+# OpenCL may be enabled by default in OpenCV3; disable it because it's not
+# thread safe and causes unwanted GPU memory allocations.
+cv2.ocl.setUseOpenCL(False)
+
+
+DEFAULT_CONFIG = '/home/ubuntu/detectron/configs/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml'
+DEFAULT_IMG_EXT = 'jpg'
+DEFAULT_WTS = 'https://dl.fbaipublicfiles.com/detectron/35861858/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml.02_32_51.SgT4y1cO/output/train/coco_2014_train:coco_2014_valminusminival/generalized_rcnn/model_final.pkl' 
+DEFAULT_OUT_EXT = 'jpg'
+DEFAULT_THRESH = 0.5
+DEFAULT_PRIVACY_THRESH = 0.5
+DEFAULT_KP_THRESH = 2.0
+PROFILE = False
+DEBUG = False
+
+def error(msg):
+    sys.stderr.write("[{}] {}\n".format(colored('erro', 'red'), msg))
+    sys.exit(1)
+def warn(msg):
+    sys.stderr.write("[{}] {}\n".format(colored('warn', 'yellow'), msg))
+    sys.stdout.flush()
+def info(msg):
+    sys.stdout.write("[{}] {}\n".format(colored('info', 'green'), msg)) 
+    sys.stdout.flush()
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Track unique objets in video')
+    parser.add_argument(
+        '--cfg',
+        dest='cfg',
+        help='cfg model file (/path/to/model_config.yaml)',
+        default=DEFAULT_CONFIG,
+        type=str
+    )
+    parser.add_argument(
+        '--wts',
+        dest='weights',
+        help='weights model file (/path/to/model_weights.pkl)',
+        default=DEFAULT_WTS,
+        type=str
+    )
+    parser.add_argument(
+        '--output-dir',
+        dest='output_dir',
+        help='directory for visualization pdfs (default: /tmp/infer_simple)',
+        default='/tmp/infer_simple',
+        type=str
+    )
+    parser.add_argument(
+        '--image-ext',
+        dest='image_ext',
+        help='image file name extension (default: jpg)',
+        default=DEFAULT_IMG_EXT,
+        type=str
+    )
+    parser.add_argument(
+        '--always-out',
+        dest='out_when_no_box',
+        help='output image even when no object is found',
+        action='store_true'
+    )
+    parser.add_argument(
+        '--output-ext',
+        dest='output_ext',
+        help='output image file format (default: pdf)',
+        default=DEFAULT_OUT_EXT,
+        type=str
+    )
+    parser.add_argument(
+        '--thresh',
+        dest='thresh',
+        help='Threshold for visualizing detections',
+        default=DEFAULT_THRESH,
+        type=float
+    )
+    parser.add_argument(
+        '--privacy-thresh',
+        dest='privacy_thresh',
+        help='Threshold for detecting objects that should be denatured',
+        default=DEFAULT_PRIVACY_THRESH,
+        type=float
+    )
+    parser.add_argument(
+        '--kp-thresh',
+        dest='kp_thresh',
+        help='Threshold for visualizing keypoints',
+        default=DEFAULT_KP_THRESH,
+        type=float
+    )
+    parser.add_argument(
+        '--start',
+        help="Beginning timestamp of video to use, format: 00:00:00.0"
+    )
+    parser.add_argument(
+        '--length',
+        help="Length of video clip, format: 00:00:00.0"
+    )
+    parser.add_argument(
+        '--clip',
+        help="If provided, clip the video using required --start and --length arguments",
+        action='store_true'
+    )
+    parser.add_argument(
+        '--sample-rate',
+        help="How frequently to sample frames from the video",
+        required=True
+    )
+    parser.add_argument(
+        '--rebuild-rate',
+        help="FPS of the final denatured video"
+    )
+    parser.add_argument(
+        '--track-class',
+        help="Class of objects to track",
+        type=(lambda s : s.replace('[','').replace(']','').split(",")),
+        required=True
+    )
+    #parser.add_argument(
+    #    '--query-class',
+    #    help="List of classes of objects to query for, comma-separated",
+    #    required=True,
+    #    type=(lambda s : s.replace('[','').replace(']','').split(","))
+    #)
+    parser.add_argument(
+        '--nickname',
+        help="Name for this run, stored in /tmp/track",
+        required=True
+    )
+    parser.add_argument(
+        'video', help='video', default=None
+    )
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+    return parser.parse_args()
+
+TMP_DIR = '/tmp/track'
+
+def main(args):
+    logger = logging.getLogger(__name__)
+
+    merge_cfg_from_file(args.cfg)
+    cfg.NUM_GPUS = 1
+    args.weights = cache_url(args.weights, cfg.DOWNLOAD_CACHE)
+    assert_and_infer_cfg(cache_urls=False)
+
+    assert not cfg.MODEL.RPN_ONLY, \
+        'RPN models are not supported'
+    assert not cfg.TEST.PRECOMPUTED_PROPOSALS, \
+        'Models that require precomputed proposals are not supported'
+
+    model = infer_engine.initialize_model_from_cfg(args.weights)
+    dummy_coco_dataset = dummy_datasets.get_coco_dataset()
+
+    path_to_video = args.video
+
+    nickname = '.'.join(path_to_video.split("/")[-1].split(".")[:-1])
+    if args.nickname:
+        nickname = args.nickname
+
+    video_dir = os.path.join(TMP_DIR, nickname)
+    if os.path.exists(video_dir):
+        shutil.rmtree(video_dir)
+    os.makedirs(video_dir)
+
+    clipped_video = os.path.join(video_dir, "clipped.mp4")
+
+
+    if args.clip:
+        if not 'start' in args or not 'length' in args:
+            logger.error("If --clip is provided, --start and --length must also be provided")
+            sys.exit(1)
+        if len(args.start.split(":")) != 3:
+            logger.error("--start must be of the format 00:00:00")
+            sys.exit(1)
+        if len(args.length.split(":")) != 3:
+            logger.error("--length must be of the format 00:00:00.0")
+            sys.exit(1)
+
+        info("Clipping video...")
+
+        ffmpeg = Popen("ffmpeg -hide_banner -loglevel error -ss {} -i {} -c copy -t {} {}".format(
+            args.start,
+            args.video,
+            args.length,
+            clipped_video
+        ), shell=True)
+        ffmpeg.wait()
+    else:
+        clipped_video = args.video
+
+    info("Sampling video...")
+    ffmpeg = Popen("ffmpeg -i {} -vf fps={} {} -hide_banner -loglevel error".format(
+        os.path.basename(clipped_video),
+        args.sample_rate,
+        os.path.join(video_dir, "raw_%04d.jpg")
+    ), shell=True, cwd=os.path.dirname(clipped_video))
+
+    ffmpeg.wait()
+
+    im_list = sorted(glob.iglob(os.path.join(video_dir, '*.' + args.image_ext)))
+    info("Created {} frames.".format(len(im_list)))
+
+    results_json = {}
+
+    tracker = ObjectTracker()
+    ooi = {}
+    seen = set()
+
+    mot_tracker = sort.Sort()
+
+    #out = open('out.txt', 'w')
+    info("Processing frames...")
+    for im_name in tqdm(im_list, unit="frame", bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_noinv_fmt}]"):
+
+        frame_num = os.path.basename(im_name).split(".")[0].split("_")[1]
+
+        im = cv2.imread(im_name)
+        timers = defaultdict(Timer)
+        t1 = time.time()
+        with c2_utils.NamedCudaScope(0):
+            original_boxes, original_segms, original_kps = infer_engine.im_detect_all(
+                model, im, None, timers=timers
+            )
+        t2 = time.time()
+        if PROFILE:
+            for t in timers.keys():
+                print(t, timers[t].total_time, timers[t].average_time)
+            print("Total inference time: ", t2-t1)
+
+
+        if isinstance(original_boxes, list):
+            original_boxes, original_segms, original_kps, original_classes = vis_utils.convert_from_cls_format(
+                    original_boxes, original_segms, original_kps)
+        if original_boxes is None or original_boxes.shape[0] == 0 or max(original_boxes[:, 4]) < args.privacy_thresh:
+            # TODO
+            pass
+ 
+        boi = np.zeros((len(original_classes), 5), dtype=int)
+        #boi = []
+        for i in range(len(original_classes)):
+            cls = dummy_coco_dataset.classes[original_classes[i]]
+            if cls in args.track_class and original_boxes[i, -1] >= args.thresh:
+                boi[i] = original_boxes[i]
+                #boi.append(original_boxes[i])
+        box_map = tracker.update(boi)
+        track_bbs_ids = mot_tracker.update(boi)
+
+        print("===")
+        print(track_bbs_ids)
+        print("===")
+
+        ids_in_frame = set(box_map.keys())
+        new_ids = seen.symmetric_difference(ids_in_frame)
+
+        ids_fig = vis_utils.vis_sort(
+            im[:, :, ::-1],  # BGR -> RGB for visualization
+            track_bbs_ids
+        )
+        #ids_fig = vis_utils.vis_ids(
+        #    im[:, :, ::-1],  # BGR -> RGB for visualization
+        #    box_map,
+        #    new_ids
+        #)
+        seen.update(ids_in_frame)
+
+        ids_frame_name = 'ids_' + frame_num
+        ids_frame_path = os.path.join(video_dir, ids_frame_name + "." + args.output_ext)
+        ids_fig.savefig(ids_frame_path, dpi=200)
+        plt.close('all')
+
+
+    info("Rebuilding video...")
+
+    ffmpeg = Popen("ffmpeg -loglevel error -hide_banner -r {} -i {} {}".format(
+        args.rebuild_rate,
+        os.path.join(video_dir, "ids_%04d." + args.output_ext),
+        "ids.mp4"
+    ), shell=True, cwd=video_dir)
+
+    ffmpeg.wait()
+
+    info("Done.")
+
+if __name__ == '__main__':
+    workspace.GlobalInit(['caffe2', '--caffe2_log_level=1'])
+    setup_logging(__name__)
+    args = parse_args()
+    main(args)
